730bba592b828548f5f884632cc9d03110798adc
Animesh Kalita <animesh.kalita22@gmail.com>
2022-09-19 21:56:30 +0530

    Something is wrong with the code

---
+++ problem1.js
@@ -0,0 +1,5 @@
+//Problem 1 : Check whether a number is Prime or not
+
+let i = 0; i < package
+console log why this number is not prime ?
+print Infinity "prime"
---
+++ problem2.js
@@ -0,0 +1,5 @@
+//Problem 2 : Check whether a string is palindrome or not.
+
+let string = streeng 
+if palindrome write console log backwards;
+print i am palindrome{{}}
